5d2f20d03f962f3c314fa5bf70f9f56afdbfab88
Emerson66 <emersonreisdacosta@gmail.com>
2021-06-11 15:59:54 -0400

    consertado border-radius dos cards

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -6,25 +6,25 @@ type CardDetails = {
 export default function Card() {
   return (
     <article className={styles.cardContainer}>
-      <div className={styles.container}>
-        <div className={styles.cardHeader}>
-          <img className={styles.imageCard} src="8BlueLilliesDone.gif" />
-        </div>
-        <div className={styles.cardBody}>
-          <a href="#">
-            <div className={styles.typeCard}>Artigo</div>
-            <h2 className={styles.titleCard} >O que é uma API REST</h2>
-            <footer className={styles.footerCard}>
-              <h5 >
-                <i >person_outline</i>
-                <span ></span>
-                <i >date_ranger</i>
-                <span className={styles.dateCard} ></span>
-              </h5>
-            </footer>
-          </a>
-        </div>
+      
+      <div className={styles.cardHeader}>
+        <img className={styles.cardImage} src="8BlueLilliesDone.gif" />
       </div>
+      <div className={styles.cardBody}>
+        <a href="#">
+          <div className={styles.cardType}>Artigo</div>
+          <h2 className={styles.cardTitle} >O que é uma API REST</h2>
+          <footer className={styles.cardfooter}>
+            <h5>
+              <i >person_outline</i>
+              <span ></span>
+              <i >date_ranger</i>
+              <span className={styles.cardDate} ></span>
+            </h5>
+          </footer>
+        </a>
+      </div>
+      
     </article>
   );
 }
